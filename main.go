ff136711078eb3f648e0183df610fbc66af202d2
xiemalin <xiemalin@baidu.com>
2021-07-27 21:35:39 +0800

    add -embed flag

--- main.go
+++ main.go
@@ -13,14 +13,15 @@ import (
 )
 
 var (
-	datadir = flag.String("datadir", "./data", "data file path")
-	listen  = flag.String("http", ":8080", "host and port to listen. eg :8080")
+	datadir   = flag.String("datadir", "./data", "data file path")
+	listen    = flag.String("http", ":8080", "host and port to listen. eg :8080")
+	embedMode = flag.Bool("embed", true, "if true read all static file in binary executte file")
 )
 
 func main() {
 
 	flag.Parse()
-	web.Use_Embed_Mode = false
+	web.Use_Embed_Mode = *embedMode
 	module, err := web.NewWebModule(*listen, "/", *datadir)
 	if err != nil {
 		fmt.Println(err)
